d2f42d22a6c92d5e46813dc5ce1d20bdca9a1948
tristana <cristinam.ignat@gmail.com>
2014-01-03 22:11:36 +0200

    Added En version, reworked Ro version: menu items are no longer images, header is on everypage

---
+++ war/lib/shuffleImg.js
@@ -0,0 +1,33 @@
+﻿(function($) {
+
+	$.randomImage = {
+		defaults : {
+			// you can change these defaults to your own preferences.
+			path : '../images/', // change this to the path of your images
+			myImages : [ 'main001.jpg', 'main002.jpg', 'main003.jpg',
+					'main004.jpg', 'main005.jpg' ]
+		}
+	}
+
+	$.fn.extend({
+		randomImage : function(config) {
+
+			var config = $.extend({}, $.randomImage.defaults, config);
+
+			return this.each(function() {
+				var imageNames = config.myImages;
+				var imageNamesSize = imageNames.length;
+				var lotteryNumber = Math.floor(Math.random() * imageNamesSize);
+				var winnerImage = imageNames[lotteryNumber];
+				var fullPath = config.path + winnerImage;
+				$(this).attr({
+					src : fullPath,
+					alt : winnerImage
+				});
+
+			});
+		}
+
+	});
+
+})(jQuery);
